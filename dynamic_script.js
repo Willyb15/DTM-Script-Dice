37f2a81607e2eba2b58a94d18d0add31b67c3cfb
Will Bryant <william.steen.bryant@gmail.com>
2018-11-05 13:15:12 -0700

    adding files

---
+++ dynamic_script.js
@@ -0,0 +1,7 @@
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script"); //Make a script DOM node
+    script.src = url; //Set it's src to the provided URL
+    document.head.appendChild(script); //Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+
+dynamicallyLoadScript("//static.criteo.net/js/ld/ld.js");
